5c037fa495a40395d1755046f0334c84aab9a061
cryptokkk <mrtosin.obasa@gmail.com>
2022-07-19 10:00:10 +0100

    first commit

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.hardhatLocal,
+  mainnet: Networks.polygonMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'SuperCryptoEggs',
+  tokenName: 'Super Crypto Eggs',
+  tokenSymbol: 'SCE',
+  hiddenMetadataUri: "ipfs://QmaqHw8bazecWQcasCXszcvuSJN8DvxXosM8EvAUG4yjDd/",
+  maxSupply: 999,
+  whitelistSale: {
+    price: 0.03,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: '0xEfdd9061769820288a2E3F0a1Bfacc952Aedc104',
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
